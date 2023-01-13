ded7b91701d4b13ca38c036edab5f47e4e51f138
momenmarwan <101040430+momenmarwan@users.noreply.github.com>
2023-01-13 21:01:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from 'body-parser';
+import userRouter from "./routes/users.js";
+
+const app = express();
+const  PORT = 5000 ;    
+
+app.use(bodyParser.json());
+app.use('/users' , userRouter)
+
+app.get('/' , (req , res) => res.send("hello from home page")) ;
+
+app.listen(PORT , () => console.log(`server running on port : http://localhost:${PORT}`));
+
+
